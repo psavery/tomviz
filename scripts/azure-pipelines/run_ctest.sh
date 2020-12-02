f2c87a1252678f4078cf59a2b519b9baef24e4c0
Patrick Avery <patrick.avery@kitware.com>
2020-12-02 11:04:12 -0600

    Try setting windows python test executable...
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- scripts/azure-pipelines/run_ctest.sh
+++ scripts/azure-pipelines/run_ctest.sh
@@ -13,6 +13,7 @@ elif [[ $AGENT_OS == 'Windows_NT' ]]; then
   # The azure-pipelines docker on Windows can't run Linux images
   # If we find a way to get it to work, we can include this test
   CTEST_EXTRA_ARGS='-E DockerUtilities'
+  export TOMVIZ_TEST_PYTHON_EXECUTABLE=$(which python)
 fi
 
 cd $BUILD_BINARIESDIRECTORY
